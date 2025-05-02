196a25ac2fbae096a49e4d1725f521d60ff6020a
patelkenil26 <patelkenil2353@gmail.com>
2025-05-02 14:09:48 +0530

    add basic of c++

---
+++ 1. Basic of Programming - Lavel 1/2. Basics of C++/1-first.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Namaste dunia" <<endl;
+    cout<< 4 << 5 << 7 << endl;
+
+
+    int age;
+    cout << "Enter your age : ";
+    cin >> age;
+    cout << " your age is : " << age << endl;
+
+    bool found = true;
+    cout << found << endl;
+
+    // endl ulternative \n
+    cout << "Kenil Patel\n";
+    cout << "Good Morning";
+
+
+    return 0;
+}
+
